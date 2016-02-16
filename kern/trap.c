2e02713ab71496899c8a468241a7a8ae300b86ed
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2016-02-16 12:01:58 +0800

    add picirq from lab7

--- kern/trap.c
+++ kern/trap.c
@@ -199,6 +199,12 @@ trap_dispatch(struct Trapframe *tf)
 							tf->tf_regs.reg_edi,
 							tf->tf_regs.reg_esi);
 			return;
+		case IRQ_OFFSET+IRQ_SERIAL:
+			serial_intr();
+			return;
+		case IRQ_OFFSET+IRQ_KBD:
+			kbd_intr();
+			return;
 		default:
 			log3("default dispatch");
 	}
